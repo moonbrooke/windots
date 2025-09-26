a6bc04723216ee45af8b9410307b1e56dbc57eab
moonbrooke <smoeland@gmail.com>
2025-09-26 10:37:32 +0700

    removed bat file, symlink doesn't work for windows terminal settings.json :(

--- terminal.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-setlocal
-
-set "LINK=%USERPROFILE%\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json"
-set "TARGET=C:\GitHub\windots\starship\starship.toml"
-
-mklink "%LINK%" "%TARGET%"
