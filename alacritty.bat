aa5eebbaf998119ba66bc619442d105f0e476138
moonbrooke <smoeland@gmail.com>
2025-11-03 08:09:51 +0700

    add alacritty conf + symlink

---
+++ alacritty.bat
@@ -0,0 +1,13 @@
+@echo off
+setlocal
+
+set "LINK=%USERPROFILE%\AppData\Roaming\alacritty"
+set "TARGET=C:\GitHub\windots\alacritty"
+
+:: Remove existing link or folder if it exists
+if exist "%LINK%" (
+    rmdir "%LINK%" 2>nul
+)
+
+:: Create directory symlink (/D switch is required for directories)
+mklink /D "%LINK%" "%TARGET%"
