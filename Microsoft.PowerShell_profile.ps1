9b14fad825c35767c4dfe7af90159c37dee32418
moonbrooke <smoeland@gmail.com>
2025-12-09 22:54:12 +0700

    pwsh: add choco

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -12,5 +12,15 @@ Import-Module -Name Terminal-Icons
 Set-Alias c Clear-Host
 Set-Alias stow "$PSScriptRoot\Stow.ps1"
 
+# Import the Chocolatey Profile that contains the necessary code to enable
+# tab-completions to function for `choco`.
+# Be aware that if you are missing these lines from your profile, tab completion
+# for `choco` will not function.
+# See https://ch0.co/tab-completion for details.
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
+
 # PUT AT THE END
 Invoke-Expression (&starship init powershell)
