63e9aaaff77920100514cbfe2eacbdaf3ba98f59
moonbrooke <smoeland@gmail.com>
2025-12-25 23:13:27 +0700

    pwsh: refactor

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -27,48 +27,4 @@ if (Test-Path($ChocolateyProfile)) {
 # Invoke-Expression (&starship init powershell)
 
 # Custom PowerShell Prompt 
-function prompt {
-    $cwd = (Get-Location).Path
-
-    $gitInfo = ""
-    $dirty = $false
-    $unpushed = $false
-    try {
-        $gitDir = git rev-parse --show-toplevel 2>$null
-        if ($gitDir) {
-            $repoName = Split-Path $gitDir -Leaf
-            $branch   = git rev-parse --abbrev-ref HEAD 2>$null
-
-            $status = git status --porcelain 2>$null
-            if ($status) { $dirty = $true }
-
-            try {
-                $ahead = git rev-list "@{u}..HEAD" 2>$null
-                if ($ahead) { $unpushed = $true }
-            } catch {}
-
-            if ($branch) {
-                $gitInfo = "  $branch"
-            } else {
-                $gitInfo = " 󰊢 $repoName"
-            }
-        }
-    } catch {}
-
-    Write-Host "󰉋" $cwd -ForegroundColor Cyan -NoNewline
-    if ($gitInfo) {
-        Write-Host $gitInfo -ForegroundColor Magenta -NoNewline
-        if ($dirty) {
-            Write-Host " [*]" -ForegroundColor Red -NoNewline
-        }
-        if ($unpushed) {
-            Write-Host " [󰁝]" -ForegroundColor Green -NoNewline
-        }
-        Write-Host ""
-    } else {
-        Write-Host ""
-    }
-
-    Write-Host "$" -ForegroundColor Gray -NoNewline
-    return " "
-}
+. "$env:USERPROFILE\Documents\PowerShell\Prompt.ps1"
---
+++ Prompt.ps1
@@ -0,0 +1,45 @@
+function prompt {
+    $cwd = (Get-Location).Path
+
+    $gitInfo = ""
+    $dirty = $false
+    $unpushed = $false
+    try {
+        $gitDir = git rev-parse --show-toplevel 2>$null
+        if ($gitDir) {
+            $repoName = Split-Path $gitDir -Leaf
+            $branch   = git rev-parse --abbrev-ref HEAD 2>$null
+
+            $status = git status --porcelain 2>$null
+            if ($status) { $dirty = $true }
+
+            try {
+                $ahead = git rev-list "@{u}..HEAD" 2>$null
+                if ($ahead) { $unpushed = $true }
+            } catch {}
+
+            if ($branch) {
+                $gitInfo = "  $branch"
+            } else {
+                $gitInfo = " 󰊢 $repoName"
+            }
+        }
+    } catch {}
+
+    Write-Host "󰉋" $cwd -ForegroundColor Cyan -NoNewline
+    if ($gitInfo) {
+        Write-Host $gitInfo -ForegroundColor Magenta -NoNewline
+        if ($dirty) {
+            Write-Host " [*]" -ForegroundColor Red -NoNewline
+        }
+        if ($unpushed) {
+            Write-Host " [󰁝]" -ForegroundColor Green -NoNewline
+        }
+        Write-Host ""
+    } else {
+        Write-Host ""
+    }
+
+    Write-Host "$" -ForegroundColor Gray -NoNewline
+    return " "
+}
